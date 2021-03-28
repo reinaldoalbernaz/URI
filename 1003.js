d0f096643ec0c3d84d1d1a3f7a7d4adaf2730fea
Reinaldo <reinaldoalbernaz@gmail.com>
2021-03-28 17:07:38 -0300

    1003

---
+++ 1003.js
@@ -0,0 +1,8 @@
+// https://www.urionlinejudge.com.br/judge/pt/problems/view/1003
+
+let lines = [-30, 10];
+
+const a = parseInt(lines.shift());
+const b = parseInt(lines.shift());
+const SOMA = a + b;
+console.log('SOMA = ', SOMA);
