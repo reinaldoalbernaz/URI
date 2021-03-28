79e8b1676d816ac06ef7912df3ef324200889592
Reinaldo <reinaldoalbernaz@gmail.com>
2021-03-28 17:07:26 -0300

    1001

---
+++ 1001.js
@@ -0,0 +1,8 @@
+// https://www.urionlinejudge.com.br/judge/pt/problems/view/1001
+
+let lines = [5,10];
+
+let a = parseInt(lines.shift());
+let b = parseInt(lines.shift());
+
+console.log('X = ' + (a+b));
