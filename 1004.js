fdfca6a1094c36cc391a0cbe9fddf532a9cc7f6a
Reinaldo <reinaldoalbernaz@gmail.com>
2021-03-28 17:07:47 -0300

    1004

---
+++ 1004.js
@@ -0,0 +1,8 @@
+// https://www.urionlinejudge.com.br/judge/pt/problems/view/1004
+
+let lines = [3, 9];
+
+const a = parseInt(lines.shift());
+const b = parseInt(lines.shift());
+const PROD = a * b;
+console.log('PROD =', PROD);
