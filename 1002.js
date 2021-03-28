c0d7ba28361521e87fd2ac772c55e8b7cd240f51
Reinaldo <reinaldoalbernaz@gmail.com>
2021-03-28 17:07:33 -0300

    1002

---
+++ 1002.js
@@ -0,0 +1,6 @@
+// https://www.urionlinejudge.com.br/judge/pt/problems/view/1002
+
+let lines = [10.00000];
+
+let raio = parseFloat(lines.shift()).toFixed(2);
+console.log('A=' + (3.14159*(raio**2)).toFixed(4));
